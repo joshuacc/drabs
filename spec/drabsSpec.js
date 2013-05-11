98587a66690ff255567163d769378ff694e39fbb
Josh Clanton <josh.clanton@naviance.com>
2013-05-11 00:16:28 -0400

    Added deepGet. Fixes #3.

--- spec/drabsSpec.js
+++ spec/drabsSpec.js
@@ -2,4 +2,34 @@ describe("Drabs", function() {
     it("should exist", function() {
         expect(typeof drabs).toBe("object");
     });
+
+    describe("deepGet", function() {
+        var deepObject = {
+            a: {
+                b: {
+                    c: "c"
+                }
+            },
+            "falseVal": false,
+            "nullVal": null
+        };
+
+        it("should get a deep property's value", function() {
+            expect(drabs.deepGet(deepObject, ["a", "b", "c"])).toEqual("c");
+            expect(drabs.deepGet(deepObject, ["falseVal"])).toEqual(false);
+        });
+
+        it("should return undefined for non-values (including null)", function() {
+            expect(drabs.deepGet(deepObject, ["a", "notHere"])).toBeUndefined();
+            expect(drabs.deepGet(deepObject, ["nullVal"])).toBeUndefined();
+        });
+
+        it("should return a default value instead of undefined if one is provided", function() {
+            expect(drabs.deepGet(deepObject, ["nullVal"], "defaultVal")).toBe("defaultVal");
+        });
+
+        it("should accept a 'dot notation' string as a property list", function() {
+            expect(drabs.deepGet(deepObject, "a.b.c")).toEqual("c");
+        });
+    });
 });
--- src/drabs.js
+++ src/drabs.js
@@ -2,4 +2,36 @@
 
     root.drabs = {};
 
+    drabs.deepGet = function (obj, props, defaultValue) {
+        // If the property list is in dot notation, convert to array
+        if (typeof props === "string") {
+            props = props.split(".");
+        }
+
+        // In order to avoid constantly checking the type of the properties
+        // we separate the real logic out into an inner function.
+        var deepGetByArray = function (obj, propsArray, defaultValue) {
+            // If we have reached an undefined/null property
+            // then stop executing and return the default value.
+            // If no default was provided it will be undefined.
+            if (obj === undefined || obj === null) {
+                return defaultValue;
+            }
+
+            // If the path array has no more elements, we've reached
+            // the intended property and return its value
+            if (propsArray.length === 0) {
+                return obj;
+            }
+
+            // Prepare our found property and path array for recursion
+            var foundSoFar = obj[propsArray[0]];
+            var remainingProps = propsArray.slice(1);
+
+            return deepGetByArray(foundSoFar, remainingProps, defaultValue);
+        };
+
+        return deepGetByArray(obj, props, defaultValue);
+    };
+
 })(this);
